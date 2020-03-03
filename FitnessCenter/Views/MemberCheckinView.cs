40417283ef820addbdc78c6d1e6dd470fa672e40
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-03-02 19:38:58 -0500

    Updated billing

--- FitnessCenter/Views/MemberCheckinView.cs
+++ FitnessCenter/Views/MemberCheckinView.cs
@@ -6,29 +6,33 @@ using System.Text;
 
 namespace FitnessCenter.Views
 {
-    public class MemberCheckinView
+    public static class MemberCheckinView
     {
+        public static int MonthDifference(this DateTime lValue, DateTime rValue)
+        {
+            int monthResult = (lValue.Month - rValue.Month) + 12 * (lValue.Year - rValue.Year);
+            return monthResult;
+        }
         public static void Display()
         {
             Console.Clear();
             Console.WriteLine($"Member ID: {HomeController.currentMember.Id}");
             Console.WriteLine($"Member Name:{HomeController.currentMember.Name}");
             Console.WriteLine($"Home Club: {HomeController.currentMember.Membership}");
-            
-            
+
             MCMember placeHolder = (MCMember)HomeController.currentMember;
-            TimeSpan remainder = DateTime.Today.Subtract(placeHolder.Begin);
-            TimeSpan billing = remainder * placeHolder.Price;
+            int billing = MonthDifference(DateTime.Today, placeHolder.Begin) * placeHolder.Price;
 
             if (HomeController.currentMember.Membership == Membership.MultiClub)
             {
-                
+
                 Console.WriteLine("Welcome!");
                 Console.WriteLine($"Points: {placeHolder.Points}\n");
                 Console.WriteLine($"Membership Price: {placeHolder.Price}");
                 Console.WriteLine($"Start Date: {placeHolder.Begin}");
                 Console.WriteLine($"Current Date: {DateTime.Today}\n");
-                Console.WriteLine($"Your current balance is: {billing}");
+
+                Console.WriteLine($"Your current amount due is:{billing} ");
             }
             else
             {
@@ -36,7 +40,8 @@ namespace FitnessCenter.Views
                 Console.WriteLine($"Membership Price: {placeHolder.Price}");
                 Console.WriteLine($"Start Date: {placeHolder.Begin}");
                 Console.WriteLine($"Current Date: {DateTime.Today}\n");
-                Console.WriteLine($"Your current balance is: {billing}");
+
+                Console.WriteLine($"Your current amount due is: {billing}");
 
             }
             Console.WriteLine("Press any key to continue.");
